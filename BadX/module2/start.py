5ffea4cd8ee2b4df055dc1fac7b623f84f47af67
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2025-03-16 12:47:45 +0530

    Update start.py

--- BadX/module2/start.py
+++ BadX/module2/start.py
@@ -1,1 +1,44 @@
+if START_PIC:
+    START_PIC = START_PIC
+else:
+    START_PIC = "https://telegra.ph/file/6482940720892cb9a4479.jpg"
 
+
+@Client.on_message(filters.command(["start"], prefixes=HANDLER))
+async def _start(Badmunda: Client, message: Message):
+    global START_MESSAGE
+    my_detail = await Badmunda.get_me()
+    my_mention = my_detail.mention
+    if START_MESSAGE:
+        START_MESSAGE = START_MESSAGE
+    else:
+        START_MESSAGE = f"ʜᴇʏ💫 {message.from_user.mention}🌸\n✥ ɪ ᴀᴍ {my_mention}\n\n❖━━━━•❅•°•❈•°•❅•━━━━❖\n\n✥ **__ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ__** = {py_version}\n✥ **__ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ__** = {platform.python_version()}\n✥ **__ʙᴏᴛsᴘᴀᴍ ᴠᴇʀsɪᴏɴ__** = {version}\n\n❖━━━━•❅•°•❈•°•❅•━━━━❖"
+    if ".jpg" in START_PIC or ".png" in START_PIC:
+        for i in range(1, 26):
+            lol = globals()[f"Client{i}"]
+            if lol is not None:
+                await lol.send_photo(
+                    message.chat.id,
+                    START_PIC,
+                    caption=START_MESSAGE,
+                    reply_markup=InlineKeyboardMarkup(await start_cmd(Badmunda)),
+                )
+    elif ".mp4" in START_PIC.lower():
+        for i in range(1, 26):
+            lol = globals()[f"Client{i}"]
+            if lol is not None:
+                await lol.send_video(
+                    message.chat.id,
+                    START_PIC,
+                    caption=START_MESSAGE,
+                    reply_markup=InlineKeyboardMarkup(await start_cmd(Badmunda)),
+                )
+    else:
+        for i in range(1, 26):
+            lol = globals()[f"Client{i}"]
+            if lol is not None:
+                await lol.send_message(
+                    message.chat.id,
+                    START_MESSAGE,
+                    reply_markup=InlineKeyboardMarkup(await start_cmd(Badmunda)),
+                )

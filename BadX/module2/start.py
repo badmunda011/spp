85077a9a8887d380ec20441b2a730f171b7be4fc
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2025-03-16 13:56:24 +0530

    Update start.py

--- BadX/module2/start.py
+++ BadX/module2/start.py
@@ -1,1 +1,13 @@
+import datetime
+import time
+import asyncio
 
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from pyrogram.enums import ChatType, ChatMemberStatus
+
+from . import TheBotX
+
+@Client.on_message(
+    filters.command("ping", prefixes=TheBotX.handler) #& filters.user(TheBotX.sudo.sudoUsers)
+)

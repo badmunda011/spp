e16186382bd6806adc15e3185ea4671eef686fea
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2025-03-23 10:32:24 +0530

    Update help.py

--- BadX/module/help.py
+++ BadX/module/help.py
@@ -1,1 +1,10 @@
+from . import TheBadX
+from pyrogram import Client, filters
 
+@Client.on_message(
+    filters.command("help", , prefixes=TheBadX.handler)
+)
+async def hi(_, message):
+    if await TheBadX.sudo.sudoFilter(message, 3): #sudo filter
+        return
+    await message.reply("Hello")

3a0318702a887577d516f8a7a73b4342715d9ee0
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2025-03-23 10:36:39 +0530

    Update keyboard.py

--- BadX/help/keyboard.py
+++ BadX/help/keyboard.py
@@ -14,7 +14,6 @@ start_keyboard = ReplyKeyboardMarkup(
     placeholder="Please Select",
     resize_keyboard=True,
 )
-)
 
 # --- funcs --- #
 def btn(text, value, type="callback_data") -> InlineKeyboardButton:
@@ -29,39 +28,3 @@ def gen_inline_keyboard(collection: list, row: int = 2) -> list[list[InlineKeybo
             kyb.append(button)
         keyboard.append(kyb)
     return keyboard
-
-# ---- Inline ---- #
-help_buttons = InlineKeyboardMarkup(
-    [
-        [
-            InlineKeyboardButton("🔹 Basic", "help:basic"),
-            InlineKeyboardButton("Spam 🔹", "help:spam")
-        ],
-        [
-            InlineKeyboardButton("🔹 DirectMessage (DM)", "help:direct"),
-            InlineKeyboardButton("Raid 🔹", "help:Raid")
-        ],
-        [
-            InlineKeyboardButton("🔹 Profile", "help:profile"),
-            InlineKeyboardButton("Extra 🔹", "help:extra")
-        ],
-        [
-            InlineKeyboardButton("🗑️", "client:close")
-        ]
-    ]
-)
-
-@Client.on_inline_query(filters.regex("help_menu"))
-async def inline_help(client: Client, inline_query):
-    buttons, _ = await gen_inline_help_buttons(0, sorted(Config.CMD_MENU.keys()))
-    help_text = await help_template("Owner Name", (len(Config.CMD_MENU), len(Config.BOT_CMD_MENU)), (0, 1))
-    results = [
-        InlineQueryResultPhoto(
-            id="help_menu",
-            photo_url="https://files.catbox.moe/paxel7.jpg",
-            thumb_url="https://files.catbox.moe/paxel7.jpg",
-            caption=help_text,
-            reply_markup=InlineKeyboardMarkup(buttons),
-        )
-    ]
-    await inline_query.answer(results, cache_time=0)
